96e647cb2fcce3f225e76a3b363c0968408451e9
igorpieruccini <igorpieruccini@gmail.com>
2024-01-15 16:59:10 +0100

    Add js docs

--- src/uglify-all-files-from-entry-point.js
+++ src/uglify-all-files-from-entry-point.js
@@ -1,6 +1,22 @@
 import uglify from "./uglify.js";
 import getImportStatementsUrlFromFile from "./get-import-statements-url-from-file.js";
 
+/**
+  * Uglify all files dependent of the entry point file provided
+  * for example: if you provide a index.js that imports other 2 files
+  * if will identify the dependency and uglify these dependencies too recursively.
+  *
+  * @param {string} entryPoint - the path to a file
+  * @returns {object} Object key = file path, and value = uglified code
+  * @example
+  // example of the returning object
+  {
+      "./src/index.js" : "the uglify code",
+      "./src/read-content-from-file": "the uglify code",
+  }
+  *
+  *
+  */
 export default async (entryPoint) => {
   let uglifiedContentMap = {};
 
--- src/uglify.js
+++ src/uglify.js
@@ -1,6 +1,11 @@
 import UglifyJs from "uglify-js";
 import { readContentFromFile } from "../src/read-content-from-file.js";
 
+/**
+  * Uglify js content
+  * @param {string} path - path of the file
+  * @returns `Promise<string>`
+  */
 export default async (path) => {
   const code = await readContentFromFile(path, "string");
   const result = UglifyJs.minify(code);
